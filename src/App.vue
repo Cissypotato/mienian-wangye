b3c8bc169348d3b8e2d97808f7d0bfe053cf58d5
cissy <1395422644@qq.com>
2020-04-16 17:48:28 +0800

    对接口完毕

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
 </template>
 
 <script>
+
 import Index from './pages/index.vue'
 
 export default {
--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import Vue from 'vue'
+import axios from 'axios'
+import qs from 'qs'
+
+
+Vue.prototype.axios = axios
+Vue.prototype.qs = qs
+
+
 import App from './App.vue'
 
 Vue.config.productionTip = false
